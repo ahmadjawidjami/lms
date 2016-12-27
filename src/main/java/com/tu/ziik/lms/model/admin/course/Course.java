e1aa36dbae0502256fe89ca388fadb7e29a4e8fe
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2016-12-27 19:58:34 +0100

    OneToMany relationship mapped between CourseCategory and Course.

--- src/main/java/com/tu/ziik/lms/model/admin/course/Course.java
+++ src/main/java/com/tu/ziik/lms/model/admin/course/Course.java
@@ -10,22 +10,35 @@ import javax.persistence.Id;
  */
 
 @Entity
-@Table(name = "course")
 public class Course {
+    private Long id;
+    //private String name;
+    private String title;
+//
+//
+ private String content;
+//
+   private String category;
+//
+   @Version
+    private Integer version;
+    private CourseCategory courseCategory;
+
+//    public Course() {
+//
+//    }
+//
+//    public Course(String title) {
+//        this.title = title;
+//    }
+//
+//    public Course(String title, CourseCategory courseCategory) {
+//        this.title = title;
+//        this.courseCategory = courseCategory;
+//    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String title;
-
-
-    private String content;
-
-    private String category;
-
-    @Version
-    private Integer version;
-
     public Long getId() {
         return id;
     }
@@ -50,6 +63,14 @@ public class Course {
         this.content = content;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
     public Integer getVersion() {
         return version;
     }
@@ -58,11 +79,84 @@ public class Course {
         this.version = version;
     }
 
-    public String getCategory() {
-        return category;
+    @ManyToOne
+    @JoinColumn(name = "course_category_id")
+    public CourseCategory getCourseCategory() {
+        return courseCategory;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCourseCategory(CourseCategory courseCategory) {
+        this.courseCategory = courseCategory;
     }
 }
+
+//@Entity
+//@Table(name = "course")
+//public class Course {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String title;
+//
+//
+//    private String content;
+//
+//    private String category;
+//
+//    @Version
+//    private Integer version;
+//
+//    //private CourseCategory courseCategory;
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//    public Integer getVersion() {
+//        return version;
+//    }
+//
+//    public void setVersion(Integer version) {
+//        this.version = version;
+//    }
+//
+//    public String getCategory() {
+//        return category;
+//    }
+//
+//    public void setCategory(String category) {
+//        this.category = category;
+//    }
+//
+//
+////    @ManyToOne
+////    @JoinColumn(name = "course_category_id")
+////    public CourseCategory getCourseCategory() {
+////        return courseCategory;
+////    }
+////
+////    public void setCourseCategory(CourseCategory courseCategory) {
+////        this.courseCategory = courseCategory;
+////    }
+//}
--- src/main/java/com/tu/ziik/lms/model/admin/course/CourseCategory.java
+++ src/main/java/com/tu/ziik/lms/model/admin/course/CourseCategory.java
@@ -1,19 +1,37 @@
 package com.tu.ziik.lms.model.admin.course;
 
 import javax.persistence.*;
+import java.util.Set;
 
 /**
  * Created by ahmadjawid on 12/23/16.
  */
+
+
 @Entity
+@Table(name = "course_category")
 public class CourseCategory {
+    private Long id;
+    private String name;
+    private Set<Course> courses;
+
+//    public CourseCategory(){
+//
+//    }
+//
+//    public CourseCategory(String name) {
+//        this.name = name;
+//    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    public Long getId() {
+        return id;
+    }
 
-    private String name;
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
@@ -23,11 +41,63 @@ public class CourseCategory {
         this.name = name;
     }
 
-    public Long getId() {
-        return id;
+    @OneToMany(mappedBy = "courseCategory", cascade = CascadeType.ALL)
+    public Set<Course> getCourses() {
+        return courses;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCourses(Set<Course> courses) {
+        this.courses = courses;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+//@Entity
+//public class CourseCategory {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String name;
+//
+//    //private Set<Course> courses;
+//
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//
+////    @OneToMany(mappedBy = "bookCategory", cascade = CascadeType.ALL)
+////    public Set<Course> getCourses() {
+////        return courses;
+////    }
+////
+////    public void setBooks(Set<Course> courses) {
+////        this.courses = courses;
+////    }
+//}
