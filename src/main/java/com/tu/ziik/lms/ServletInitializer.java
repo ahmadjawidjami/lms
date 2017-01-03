4a2bda81bf9227818564734132d24094e5385121
Ahmad Jawid Jami <ahmadjawid@wlan-141-23-206-116.tubit.tu-berlin.de>
2017-01-03 10:51:51 +0100

    working on deploy to amazon.

---
+++ src/main/java/com/tu/ziik/lms/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tu.ziik.lms;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmsApplication.class);
+	}
+
+}
